8870beca4dab17a76254f9d87d8abfd308eae592
jinyj <1406060330@qq.com>
2020-12-11 18:39:31 +0800

    20201211 课程点播视频组件demo

---
+++ pages/courseModule/courseDiscussion.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      课程讨论页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/courseModule/enterCourse.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 进入课程页面 -->
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+    
+    };
+  },
+  mounted() {},
+  methods: {
+  
+  },
+};
+</script>
+
+<style>
+
+</style>
