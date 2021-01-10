54bb978d69f447808593969eb7d7613428bdd191
jinyingjie <1406060330@qq.com>
2021-01-10 16:52:06 +0800

    20210110 第一次提交

---
+++ app说明文档.md
@@ -0,0 +1,23 @@
+# app说明文档
+
+### 整体结构
+
+### 具体文件
+
+- common文件夹
+  - 三个文件主要是配置的拦截
+- component
+  - 文件下主要是uni-app的第三方
+- pages
+  - component：存放公共的组件
+  - courseModule：存放课程部分的所有页面
+  - img：存放页面的图片
+  - index：首页
+  - login：登录和注册页面
+  - store：vuex
+- static
+  - 静态文件
+- uview-ui
+  - uview组件库
+- pages.json
+  - 配置页面顶部样式和路由
---
+++ pages/component/courseExam/courseExam.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="">
+	<view>
+		<!-- <view class="" style="width:6px;height:6px;background:green;border-radius:15px;float:left;margin-top:30.5px;margin-left:15px">
+		</view>
+		<view class="title" style="margin-top:22.5px;margin-left:10px">期末考试</view>
+		<p style="float:left;margin-left:300px;margin-top:22.5px">></p>
+		<view class="title" style="margin-top:9px;margin-left:15px;color: #999999;">未答题    总分100分</view> -->
+        <view class="" style="width:6px;height:6px;background:green;border-radius:15px;float:left;margin-top:30.5px;margin-left:15px">
+        </view>
+        <view class="title" style="margin-top:22.5px;margin-left:10px">期末考试</view>
+        <p style="float:left;margin-left:300px;margin-top:22.5px">></p>
+        <view class="title" style="margin-top:9px;margin-left:15px;color:#999999">未答题    总分100分</view>
+        <!-- <p style="float:left;margin-left:220px;margin-top:45px">2020-11-10</p> -->
+        <view class="title" style="margin-top:35px;margin-left:-120px;color:#999999">起止日期</view>
+        <p style="float:left;margin-left:-50px;margin-top:35px">2020.12.15  00:00:00-2020.12.16  23:59:59</p>
+		
+		
+	</view>
+	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.title{
+		float:left;
+	}
+	
+
+</style>
